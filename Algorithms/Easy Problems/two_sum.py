d1a45318b36889df47222f38387c09eb1d53129d
Chyrus Miguel D. Macalla <macalla.chyrusmiguel@gmail.com>
2026-01-26 17:50:15 +0800

    Implement twoSum function to find indices of two numbers

---
+++ Algorithms/Easy Problems/two_sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        complement = dict()
+
+        for i in range(len(nums)):
+            y = target - nums[i]
+            if y in complement:
+                return [i, complement[y]]
+            complement[nums[i]] = i
