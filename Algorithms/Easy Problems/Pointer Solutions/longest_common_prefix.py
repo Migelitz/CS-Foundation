e75fbc9792447019beac6923fd3078868cf645fb
Chyrus Miguel D. Macalla <macalla.chyrusmiguel@gmail.com>
2026-07-05 16:48:38 +0800

    Optimize longestCommonPrefix function
    Refactor longestCommonPrefix method to avoid sorting and improve efficiency by checking characters iteratively.

--- Algorithms/Easy Problems/Pointer Solutions/longest_common_prefix.py
+++ Algorithms/Easy Problems/Pointer Solutions/longest_common_prefix.py
@@ -1,15 +1,22 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        strs.sort()
-        LCP = ""
-        for i in range(min(len(strs[0]), len(strs[-1]))):
-            if strs[0][i] != strs[-1][i]:
-                return LCP
-            LCP += strs[0][i]
-        return LCP
+        result = ""
 
-# N = Number of strings 
-# S = max length of string
+        for i in range(len(strs[0])):
+            for j in strs:
+                if i == len(j) or strs[0][i] != j[i]:
+                    return result
+            result += strs[0][i]
 
+        return result
+
+# February 2026
 # Time Complexity: O(N * S log S) 
 # Space Complexity: O(N + S) Tim sort uses temporary space
+
+# July 5 2026
+# Time and complexity update
+# Documentary: Removed the sort() for cheaper time and space. Instead, rely on checking each strings character to first first character iteratively 
+
+# Time Complexity: O(n * m)
+# Space Complexity: O(1)
